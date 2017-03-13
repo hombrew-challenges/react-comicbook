9632e0592d65432f054a5fc8bf2f0c60043550ad
Manuel Maldonado <mmaldonadortiz92@gmail.com>
2017-03-13 19:45:47 -0400

    character boxes in progress

--- src/app/pages/Characters/character-list.js
+++ src/app/pages/Characters/character-list.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Button} from 'react-bootstrap'
 
 // components
 import {SectionTitle} from '../Components/sections'
 
 // img
 import charactersIcon from '../../../assets/images/icons/characters.png'
+import spidermanAvatar from '../../../assets/images/avatars/spiderman-avatar.jpg'
 
 export default function CharacterList() {
   return (
@@ -14,9 +15,13 @@ export default function CharacterList() {
         <Col xs={12}><SectionTitle img={charactersIcon} label="Characters"/></Col>
       </Row>
       <Row>
-        <Col xs={12} sm={6}>
-          <Character/>
-        </Col>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((e) => {
+          return (
+            <Col className="margin-bottom-30" key={e} xs={12} sm={6}>
+              <Character/>
+            </Col>
+          )
+        })}
       </Row>
     </div>
   )
@@ -24,8 +29,34 @@ export default function CharacterList() {
 
 function Character() {
   return (
-    <div>
-      Hola
+    <div className="character responsive-square">
+      <div className="character-content">
+        <Col xs={12}>
+
+          <Row>
+            <Col className="responsive-square no-padding" xs={6}>
+              <div
+                className="avatar responsive-square"
+                style={{backgroundImage: `url(${spidermanAvatar})`}}/>
+            </Col>
+
+            <Col className="responsive-square no-padding" xs={6}>
+              <div className="character-info-wrapper responsive-square-content padding-left-10 padding-right-10">
+                <div className="title">Spider Man</div>
+                <div className="content padding-bottom-10">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, necessitatibus molestiae nemo eius, obcaecati laboriosam iusto placeat voluptas consequatur saepe corrupti ad, qui nulla? Nisi laudantium quos eius possimus, in.
+                </div>
+                <Button bsStyle="danger" className="margin-left-10">View more</Button>
+              </div>
+            </Col>
+          </Row>
+
+          <Row>
+
+          </Row>
+
+        </Col>
+      </div>
     </div>
   )
 }
--- src/app/pages/Components/sections.js
+++ src/app/pages/Components/sections.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react'
 
 export function SectionTitle({img, label}) {
   return (
-    <div className="section-title">
+    <div className="section-title margin-bottom-20">
       <img className="margin-right-15" src={img} alt={label}/>
       <label>{label}</label>
     </div>
